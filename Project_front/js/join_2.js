694e055479890e99a03016095e33735dac2c5dfb
Ellui052 <89927726+Ellui052@users.noreply.github.com>
2023-05-02 13:51:43 +0900

    Create join_2.js

--- Project_front/js/join_2.js
+++ Project_front/js/join_2.js
@@ -18,6 +18,8 @@ function join() {
     alert("사용자명을 입력해주세요");
   } else if (input[1].value == "") {
     alert("비밀번호를 입력해주세요");
+  } else if (input[1].value.length < 6) {
+    alert("비밀번호는 최소 6자리 이상입니다.");
   } else if (input[2].value == "") {
     alert("비밀번호 확인을 입력해주세요");
   } else if (input[2].value != input[1].value) {
